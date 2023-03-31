95df8f57a90878a453d06f184a564f06dd49b212
Stulionus <mcapcanari@gmail.com>
2023-03-31 08:12:16 -0700

    Exercise 1

--- Exercise/Exercise1.java
+++ Exercise/Exercise1.java
@@ -15,6 +15,73 @@ public class Exercise1 {
          */
         //TODO: add your code
 
-        System.out.println("x = " + x + ", y = " + y);
+        int tempVariable;
+
+        //Test 1
+        System.out.println( "Was: " + " x = " + x + ", y = " + y);
+        tempVariable = x;
+        x=y;
+        y=tempVariable;
+        System.out.println("Now: " + "x = " + x + ", y = " + y);
+
+        x = 14;
+        y = 23;
+
+        System.out.println();
+
+        //Test 2
+        System.out.println( "Was: " + " x = " + x + ", y = " + y);
+        tempVariable = x;
+        x=y;
+        y=tempVariable;
+        System.out.println("Now: " + "x = " + x + ", y = " + y);
+
+        x = 0;
+        y = 3;
+
+        System.out.println();
+
+        //Test 3
+        System.out.println( "Was: " + " x = " + x + ", y = " + y);
+        tempVariable = x;
+        x=y;
+        y=tempVariable;
+        System.out.println("Now: " + "x = " + x + ", y = " + y);
+
+        x = 16;
+        y = 16;
+
+        System.out.println();
+
+        //Test 4
+        System.out.println( "Was: " + " x = " + x + ", y = " + y);
+        tempVariable = x;
+        x=y;
+        y=tempVariable;
+        System.out.println("Now: " + "x = " + x + ", y = " + y);
+
+        x = -4;
+        y = -7;
+
+        System.out.println();
+
+        //Test 5
+        System.out.println( "Was: " + " x = " + x + ", y = " + y);
+        tempVariable = x;
+        x=y;
+        y=tempVariable;
+        System.out.println("Now: " + "x = " + x + ", y = " + y);
+
+        x = 0;
+        y = -7;
+
+        System.out.println();
+
+        //Test 6
+        System.out.println( "Was: " + " x = " + x + ", y = " + y);
+        tempVariable = x;
+        x=y;
+        y=tempVariable;
+        System.out.println("Now: " + "x = " + x + ", y = " + y);
     }
 }
