356949818fa7c1d2b010fdd4b25a890881d660bc
Stulionus <mcapcanari@gmail.com>
2023-04-05 16:40:58 -0700

    Part 8 done

--- Lab1/Lab1/src/Car.java
+++ Lab1/Lab1/src/Car.java
@@ -1,26 +1,41 @@
 public class Car {
     private int mileage;
     private String model;
-    private String make ;
+    private String make;
 
-    public Car (String Make, String Model, int Mileage){
+    public Car(String Make, String Model, int Mileage) {
         make = Make;
         model = Model;
         mileage = Mileage;
 
     }
-    public String getMake(){
+
+    public Car(String Make, String Model) {
+        make = Make;
+        model = Model;
+
+    }
+
+    public Car(String Make) {
+        make = Make;
+
+    }
+
+    public String getMake() {
         return make;
     }
 
-    public String getModel(){
+    public String getModel() {
         return model;
     }
 
-    public int getMileage(){
+    public int getMileage() {
         return mileage;
     }
 
+    public String toString() {
+        return make + " " + model + " " + mileage;
+    }
 
 
     public static void main(String[] args) {
@@ -42,13 +57,27 @@ public class Car {
 
         // This (the Implicit Parameter) part of the lab
 
-        Car cr3 = new Car ("Toyota","RAV4",5632);
-        System.out.println(cr3.toString());
-        cr3.getAddress();
+        Car cr3 = new Car("Toyota", "RAV4", 5632);
+        System.out.println(cr3.toString()); // For these two to work method toString() must be deleted since it is from part 6 of the lab
+        cr3.getAddress();// For these two to work method toString() must be deleted since it is from part 6 of the lab
         /*
         The address is the same because when we call it with this. and when we call it through cr3.toString()
         we refer to the same object that is stored in the same place in memory therefore the address is the same/
          */
+
+
+        //6. Overriding toString() (and equals())
+
+        System.out.println();
+
+        Car cr4 = new Car("Subaru", "Forester", 6523);
+
+        System.out.println(cr4.toString());
+
+        // 8. Constructors as Methods
+
+        Car cr5 = new Car("Nissan");
+        Car cr6 = new Car("Porsche", "911");
     }
 
 
---
+++ Lab1/Lab1/src/Squaresomething.java
@@ -0,0 +1,20 @@
+public class Squaresomething {
+    static int square;
+    static double squareD;
+    static float squareF;
+
+    public static int Squaresomething(int x) {
+        square = x * x;
+        return square;
+    }
+
+    public static double Squaresomething(double x) {
+        squareD = x * x;
+        return squareD;
+    }
+
+    public static float Squaresomething(float x) {
+        squareF = x * x;
+        return squareF;
+    }
+}
